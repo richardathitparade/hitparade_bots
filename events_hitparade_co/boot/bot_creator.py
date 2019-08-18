775b3f7eaf98f67dc59933c11aad02b2755f62f8
Richard D. Ferri <richard@hitparade.co>
2019-08-17 19:51:53 -0700

    added transaction manager

--- events_hitparade_co/boot/bot_creator.py
+++ events_hitparade_co/boot/bot_creator.py
@@ -1,6 +1,7 @@
-from events_hitparade_co.bots.producer import HitParadeProducerBot
-from events_hitparade_co.messaging.messaging import MessagingQueue
 from events_hitparade_co.threads.consumer import HitParadeConsumerThread
+from events_hitparade_co.bots.producer import HitParadeProducerBot
+from events_hitparade_co.threads.transaction import HitParadeTransactionThread
+from events_hitparade_co.messaging.messaging import MessagingQueue
 from events_hitparade_co.threads.url_publisher import UrlPublisher
 
 
@@ -13,6 +14,8 @@ class HitParadeBotCreator:
             hit_parade_scrape_bot = HitParadeConsumerThread(**bot_data)
         elif bot_data['bot.type'] == 'producer':
             hit_parade_scrape_bot = HitParadeProducerBot(**bot_data)
+        elif bot_data['bot.type'] == 'transaction':
+            hit_parade_scrape_bot = HitParadeTransactionThread(**bot_data)
         elif bot_data['bot.type'] == 'publisher':
             bot_data['publisher_id'] = MessagingQueue.unique_id(global_id=True, cache_manager=bot_data['cache_manager'])
             hit_parade_scrape_bot = UrlPublisher(**bot_data)
