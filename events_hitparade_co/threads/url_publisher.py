030defa64565a74f75a20adda86976eb58e55d34
Richard D. Ferri <richard@hitparade.co>
2019-08-18 02:57:03 +0000

    simple updates

--- events_hitparade_co/threads/url_publisher.py
+++ events_hitparade_co/threads/url_publisher.py
@@ -4,6 +4,7 @@ from events_hitparade_co.utils.collections import CollectionsUtil
 from threading import Thread
 import time
 import traceback
+import json
 class UrlPublisher(Thread):
     DEFAULT_PUBLISHER_SUFFIX = '.processors'
 
@@ -22,30 +23,36 @@ class UrlPublisher(Thread):
         self.event_subscriptions[self.subscribe_event] = dict()
         self.event_subscriptions[self.subscribe_event]['subscriber'] = rc if r else None
         self.event_subscriptions[self.subscribe_event]['result'] = r if r else False
+        print('url generators are %s ' % str( self.url_generators ) )
         self.url_generator_data = list( filter( lambda f: f.get('url_event_id', None)==self.url_event_id, self.url_generators ) )[0]
         self.url_generator_data['publish_event'] = self.publish_event
         self.url_generator_data['publish_to_event'] = kwargs.get( 'publish_to_event', None )
         self.url_generator_data.update(self.__dict__)
+        self.get_state_static_prop = kwargs.get('get_state_static_prop', None)
         generator_type_id = self.url_generator_data['type_id']
-        del  self.url_generator_data['type_id']
+        del self.url_generator_data['type_id']
         self.url_generator = self.cache_manager.cache_output_component_func(type_id=generator_type_id, **self.url_generator_data)
         self.url_generator_data['type_id'] = generator_type_id
+        self.state_storage_store_prop(prop='offset_days', val=self.offset_days)
+        self.state_storage_store_prop(prop='days_back', val=self.days_back)
         print('*************************************  UrlPublisher  *************************************')
 
 
     def generate_next_url(self, **kwargs):
+        print('generate next url %s ' % json.dumps( kwargs ) )
         return self.url_generator.generate_next_url( **kwargs )
 
+    def is_republished(self, url=None):
+        return url in self.state_storage_get_prop(prop='received_urls').keys()
 
     def get_next_key(self):
         url_val = None
         for url in self.state_storage_get_prop(prop='received_urls').keys():
-            # status_value =  CollectionsUtil.GP(dict_to_get=StateStorage.get_prop(prop='received_urls') , prop_base=url, prop_name='status')
-            # if status_value and status_value==self.incoming_status_value:
             url_val = url
             received_urls = self.state_storage_get_prop(prop='received_urls')
             url_status = received_urls[url_val]
             if url_status.get('status', None) is None:
+                url_status['status'] = 'republishing'
                 self.state_storage_store_prop(prop='received_urls',val=received_urls)
                 received_urls[url_val] = url_status
                 break
@@ -74,15 +81,54 @@ class UrlPublisher(Thread):
         parent_id_property = None
         id_value = None
         parent_id_value = None
-        if len(self.received_urls) > 0:
-            url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value = self.get_next_url()
+        generate_url_args = dict()
+        item_value = self.get_next_statics_listitem()
+        if item_value:
+            id_property = item_value['id_property']
+            parent_id_property = item_value['parent_id_property']
+            generate_url_args['publish_event'] = self.publish_to_event
+            generate_url_args['publish_to_event'] = self.publish_to_event
+            generate_url_args[id_property] = item_value[id_property] if item_value[id_property] else MessagingQueue.unique_id(global_id=True, cache_manager=self.cache_manager)
+            url_value = item_value['url']
+            data_selector = self.__dict__[item_value['data_selector_id']]
+            id_value = generate_url_args[id_property]
+            parent_id_value = item_value[parent_id_property]
+            publish_to_event = self.events.get(item_value['model_event_id'], None).get('publish_to', None)
+            url_check = url_value.split('?')[0] if '?' in url_value else url_value
+            hash_value = self.get_state_static_prop(prop=url_check, default_value=None, dict_sub=None)
         else:
-            generate_url_args = dict()
             generate_url_args['publish_event'] = self.publish_to_event
             generate_url_args['publish_to_event'] = self.publish_to_event
             generate_url_args['id'] = process_id if process_id else MessagingQueue.unique_id(global_id=True, cache_manager=self.cache_manager)
-            url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value = self.generate_next_url( **generate_url_args )
+            url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value = self.generate_next_url(**generate_url_args)
+            print('next generated url value is %s ' % url_value)
+            url_check = url_value.split('?')[0] if '?' in url_value else url_value
+            hash_value = self.get_state_static_prop(prop=url_check, default_value=None, dict_sub=None)
+            # if the url has already been scraped we go to the next.
+        while not hash_value is None:
+            print('***********************************  url [%s] is already scraped *******************************************************' % url_value)
+            url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value = self.generate_next_url(**generate_url_args)
+            url_check = url_value.split('?')[0] if '?' in url_value else url_value
+            hash_value = self.get_state_static_prop(prop=url_check, default_value=None, dict_sub=None)
         return url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value
+
+    # def __get_next_url(self, process_id=None):
+    #     url_value = None
+    #     data_selector = None
+    #     publish_to_event = None
+    #     id_property = None
+    #     parent_id_property = None
+    #     id_value = None
+    #     parent_id_value = None
+    #     if len(self.received_urls) > 0:
+    #         url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value = self.get_next_url()
+    #     else:
+    #         generate_url_args = dict()
+    #         generate_url_args['publish_event'] = self.publish_to_event
+    #         generate_url_args['publish_to_event'] = self.publish_to_event
+    #         generate_url_args['id'] = process_id if process_id else MessagingQueue.unique_id(global_id=True, cache_manager=self.cache_manager)
+    #         url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value = self.generate_next_url( **generate_url_args )
+    #     return url_value, data_selector, publish_to_event, id_property, parent_id_property, id_value, parent_id_value
 
     def add_url(self, url=None,data_selector=None, publish_to=None, publish_event=None, id_property=None, parent_id_property=None, id_value=None, parent_id_value=None, status=None):
             data_obj  = dict()
@@ -134,14 +180,6 @@ class UrlPublisher(Thread):
 
         while  self.state_storage_get_prop(prop='iterator_count') < (self.state_storage_get_prop(prop='offset_days') + self.state_storage_get_prop(prop='days_back')):
             try:
-                command, obj = MessagingQueue.wait_for_msg(  id=self.id, direction='in', caller='UrlRePublisher', wait=False  )
-                if not obj is None and (  isinstance(obj,list) or  isinstance(obj,dict)  ):
-                    print(  'wait_for_msg found message %s ' % str(obj)  )
-                    self.add_all_urls(obj=obj)
-                else:
-                    if obj is None:
-                        print(' obj is none ! ')
-                    print(  'no message found for wait_for_msg '  )
                 message =  self.event_subscriptions[self.subscribe_event]['subscriber'].next_message()
                 if message and not isinstance(message['data'], int) and message['data'].decode('utf-8').isdigit():
                     process_id = message['data'].decode('utf-8')
@@ -157,6 +195,10 @@ class UrlPublisher(Thread):
                         publish_dict['scraper_process_id'] = process_id
                         publish_dict[ id_property ] =  id_value
                         publish_dict[ parent_id_property ] = parent_id_value
+                        publish_dict['publish_to_event'] = publish_to_event
+                        publish_dict['publish_event'] = self.events.get(self.events.get(self.model_event_id, None).get('url_id', None), None).get('publish_to', None)
+                        publish_dict['data_selector'] = data_selector
+                        publish_dict['output_type'] = 'cache'
                         if isinstance(push_event, list):
                             for pe in push_event:
                                 self.cache_manager.pub( event=pe, output=publish_dict, recursive=False, pid=process_id, append_pid=True)
